042f71809da1c79ad15bdc2870fc019f9efa8092
Weislife <zhangwei119893@163.com>
2019-05-11 21:24:14 +0800

    Update printRequest.js

--- app/middleware/printRequest.js
+++ app/middleware/printRequest.js
@@ -1,12 +1,18 @@
 'use strict';
+const datetime = require('../../util/datetime');
 
 /* eslint no-unused-vars: ["error", { "args": "none" }]*/
 module.exports = (options, app) => {
   return async (ctx, next) => {
     if (ctx.app.config.env === 'local') { // 本地开发环境
-      console.log(JSON.stringify(ctx.request, null, 2));
+      console.log(datetime.format(new Date()) + ': ' + JSON.stringify(ctx.request, null, 2));
     }
 
     await next();
+
+    if (ctx.app.config.env === 'local') { // 本地开发环境
+      console.log(datetime.format(new Date()) + ': ' + JSON.stringify(Object.assign({}, JSON.parse(JSON.stringify(ctx.response)), { body: ctx.response.body }), null, 2));
+    }
   };
 };
+
