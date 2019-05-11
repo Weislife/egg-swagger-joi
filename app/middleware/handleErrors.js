df696f672c9045d8acd35dd1d59c1cfa252ad98e
Weislife <zhangwei119893@163.com>
2019-05-11 21:26:34 +0800

    Update handleErrors.js

--- app/middleware/handleErrors.js
+++ app/middleware/handleErrors.js
@@ -6,8 +6,10 @@ module.exports = (options, app) => {
     try {
       await next();
     } catch (err) {
-      // 所有的异常都在 app 上触发一个 error 事件，框架会记录一条错误日志
-      ctx.app.emit('error', err, ctx);
+      if (!(err.name === 'ResponseValidationError' && ctx.request.path.indexOf('/api/v1/backend/download/dd_login') === 0)) {
+        // 所有的异常都在 app 上触发一个 error 事件，框架会记录一条错误日志
+        ctx.app.emit('error', err, ctx);
+      }
 
       if (err.name === 'RequestValidationError') {
         ctx.status = 400;
@@ -15,15 +17,16 @@ module.exports = (options, app) => {
           message: err.details[0].message.replace(/\"/gi, "'"),
         };
       } else if (err.name === 'ResponseValidationError') {
-        ctx.status = 500;
-        ctx.body = {
-          message: '返回数据格式不对, ' + err.details[0].message.replace(/\"/gi, "'"),
-          data: err.data,
-        };
+        if (ctx.request.path.indexOf('/api/v1/backend/download/dd_login') !== 0) {
+          ctx.status = 500;
+          ctx.body = {
+            message: '返回数据格式不对, ' + err.details[0].message.replace(/\"/gi, "'"),
+            data: err.data,
+          };
+        }
       } else {
         const status = err.status || 500;
         const error = err.message || 'Internal Server Error';
-
         // 从 error 对象上读出各个属性，设置到响应中
         const obj = { message: error };
         if (err.err_code) {
